601a7e6d443afaa5cf17c09a4dfd187f32cd11de
Ray Yu <s1174159@ouhk.edu.hk>
2021-11-27 02:44:32 +0800

    init

--- SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    // MARK: - Methods
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
+
 
 @main
 struct SwiftUIFirebaseChatApp: App {
+    // MARK: - Properties
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    // MARK: - Life Cycle
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
